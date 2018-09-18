cb17312dc4dc02e040869856d8cf5b9cb9ca0b1d
SajeevBabu <sajeev.b@photoninfotech.net>
2018-09-18 17:36:10 -0500

    removed security

--- src/main/java/com/stevesmedia/fileuploader/restapi/configuration/SwaggerConfig.java
+++ src/main/java/com/stevesmedia/fileuploader/restapi/configuration/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.stevesmedia.fileuploader.restapi.configuration;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -18,6 +22,21 @@ public class SwaggerConfig {
           .select()                                  
           .apis(RequestHandlerSelectors.any())              
           .paths(PathSelectors.any())                          
-          .build();                                           
+          .build()
+          .apiInfo(apiInfo());
     }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+        					"File store project serivice RESTful api", 
+        					"Apis for file operations", 
+        					"Version 2", 
+        					"Terms of service", 
+        					new Contact("Sajeev Babu", 
+        							"www.steves-media.com", 
+        							"stevesmedia@steves-media.com"), 
+        					"License of API", 
+        					"API license URL",
+        					Collections.emptyList());
+   }
 }
